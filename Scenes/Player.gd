5766ed591d693026d170c06e64bc2f7bab9961c5
dariasteam <dariasteam94@gmail.com>
2018-07-20 12:46:34 +0100

    Update

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -12,6 +12,7 @@ onready var axis = get_node ("RigidBody/Axis")
 onready var ball = get_node ("RigidBody/Axis/Group/Ball")
 onready var area = get_node ("RigidBody/Axis/Group/Area")
 onready var rigid_2 = get_node("RigidBody2")
+onready var light = get_node("RigidBody/Axis/Group/OmniLight")
 
 onready var camera_axis = get_node ("RigidBody2/CameraAxis")
 
@@ -32,20 +33,27 @@ export (int) var n_platforms_to_meteorize = 2
 var meteor_charged = true
 var meteor = false
 
-func die():
+func die():	
+	rigid.set_gravity_scale(0)
+	rigid_2.set_gravity_scale(0)
+	rigid_2.set_linear_velocity(Vector3(0,0,0))
+	rigid.set_linear_velocity(Vector3(0,0,0))
+	rigid_2.set_sleeping(true)
+	rigid.set_sleeping(true)
+	
 	die_sound.play(0)
 	die_particles.set_emitting(true)
 	ball.queue_free()
 	area.queue_free()
 	rigid.set_gravity_scale(0)	
-	rigid.set_linear_velocity(Vector3(0,0,0))
-	rigid.set_sleeping(true)
+	
+	
 	trail.set_emitting(false)
-	idle_particles.set_emitting(false)	
-	rigid_2.set_sleeping(true)
-	rigid.set_linear_velocity(Vector3(0,0,0))
+	idle_particles.set_emitting(false)		
 	get_node("Timer").start()
 	
+	
+
 func on_platform_passed():			
 	global.update_points((counter + 1) * 10)
 	global.update_progress()
@@ -57,9 +65,9 @@ func on_platform_passed():
 	if (counter == n_platforms_to_meteorize - 1):		
 		rigid.set_gravity_scale(0)
 	
-	if (counter >= n_platforms_to_meteorize):		
-		meteor_particles.set_emitting(true)		
+	if (counter >= n_platforms_to_meteorize):				
 		if (meteor_charged):
+			meteor_particles.set_emitting(true)
 			meteorize()
 			
 	rigid_2.set_sleeping(false)
@@ -79,6 +87,7 @@ func _on_set_rotation (rot):
 
 		
 func _on_Area_body_enter(body):
+	light.set_enabled(false)
 	acceleration_sound.stop()
 	if (body.is_in_group("wall")):
 		colliding = true		
@@ -101,10 +110,10 @@ func _on_Area_body_enter(body):
 			else:
 				jump_sound.play(0)
 				var aux = decal.instance()		
-				aux.set_translation(Vector3(-1.5, 0.01,-0.5))		
+				aux.set_translation(Vector3(-1.5, 0.01,-0.5))						
 				aux.rotate_z(rand_range(0, 360))
 				body.add_child(aux)
-	
+			
 			rigid.set_linear_velocity(Vector3(0,0,0))
 			rigid.apply_impulse(Vector3(0,0,0), Vector3(0,70,0))
 			splash.set_emitting(true)
@@ -116,8 +125,10 @@ func _ready():
 	ball.set_material_override(global.mat_player)
 	var color = global.mat_player.get_parameter(FixedMaterial.PARAM_DIFFUSE)
 	trail.get_material().set_parameter(FixedMaterial.PARAM_DIFFUSE, color)
+	light.set_color(1,color)
 	
 func meteorize():
+	light.set_enabled(true)
 	meteor = true
 	meteor_charged = false	
 
