3f4037ab67e194e64f69265412bf1a4d714f91fd
dariasteam <dariasteam94@gmail.com>
2018-12-24 05:25:30 +0000

    Incremental levels

--- platforms_deployer.gd
+++ platforms_deployer.gd
@@ -16,7 +16,16 @@ func instance_in (element, i):
 func _ready():		
 	var prev_platform = first_platform.instance()
 	instance_in(prev_platform, 0)		
+
+	if (global.level > 16):
+		n_platforms += ((global.level - 16) / 2) 
+		global.level_size = n_platforms
+		
+	col.set_scale(Vector3(col.get_scale().x, col.get_scale().y * n_platforms, col.get_scale().z))
+	col.set_translation(Vector3 (0, -(n_platforms * space / 2) + 0.5, 0))
 	
+	print (-(n_platforms * space))
+		
 	for i in range (1, n_platforms):
 		var new_platform = platform.instance();
 		prev_platform.next_platform = new_platform
